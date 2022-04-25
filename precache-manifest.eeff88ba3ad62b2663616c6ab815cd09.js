dd2b3684b5a4820369134ab7b093eb170947e09b
Code Sandbox <hello@codesandbox.io>
2022-04-25 19:45:26 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.eeff88ba3ad62b2663616c6ab815cd09.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0115ce0b0e8dfda41318",
+    "url": "https://ohmegamat.github.io/csb-2ybgn/static/css/main.b664d208.chunk.css"
+  },
+  {
+    "revision": "0115ce0b0e8dfda41318",
+    "url": "https://ohmegamat.github.io/csb-2ybgn/static/js/main.27f49685.chunk.js"
+  },
+  {
+    "revision": "2943b420efda2032e5bd",
+    "url": "https://ohmegamat.github.io/csb-2ybgn/static/js/runtime~main.50db65db.js"
+  },
+  {
+    "revision": "3d43381e18b165e145fc",
+    "url": "https://ohmegamat.github.io/csb-2ybgn/static/css/2.e9cc782e.chunk.css"
+  },
+  {
+    "revision": "3d43381e18b165e145fc",
+    "url": "https://ohmegamat.github.io/csb-2ybgn/static/js/2.b61f8755.chunk.js"
+  },
+  {
+    "revision": "b632fa50a1c2baee684c57ae87bfef6b",
+    "url": "https://ohmegamat.github.io/csb-2ybgn/index.html"
+  }
+];
